e0ed4663794dd1a82aac5914a42b7eff79b3c10a
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-04 12:39:35 +0500

    page product dynamic through model. closes #13.

--- src/main/java/ahmad/thymeleaf/controllers/ProductController.java
+++ src/main/java/ahmad/thymeleaf/controllers/ProductController.java
@@ -1,14 +1,31 @@
 package ahmad.thymeleaf.controllers;
 
+import ahmad.thymeleaf.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping(value = {"/product"}, method = RequestMethod.GET)
     public String getProduct(){
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = {"/product/{id}"}, method = RequestMethod.GET)
+    public String getProductById(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.getProduct(id));
+
         return "product";
     }
 }
