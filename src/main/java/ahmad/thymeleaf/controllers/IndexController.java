8156755831294a060b9fb653dc0a8f359edee7b4
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-05 12:05:51 +0500

    page index dynamic through model. closes #14.

--- src/main/java/ahmad/thymeleaf/controllers/IndexController.java
+++ src/main/java/ahmad/thymeleaf/controllers/IndexController.java
@@ -1,14 +1,24 @@
 package ahmad.thymeleaf.controllers;
 
+import ahmad.thymeleaf.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
+    private final ProductService productService;
+
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping(value = {"/"}, method = RequestMethod.GET)
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAttribute("products", productService.listProducts());
+
         return "index";
     }
 }
