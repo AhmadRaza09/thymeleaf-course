7e755a5df305e3943dd4720f2fe0db0cd27f52a1
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-07-06 16:12:00 +0500

    added login form page and validation. closes #17.

---
+++ src/main/java/ahmad/thymeleaf/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package ahmad.thymeleaf.controllers;
+
+import ahmad.thymeleaf.commands.LoginCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+    public String showLoginForm(Model model){
+
+        model.addAttribute("loginCommand", new LoginCommand());
+        return "loginform";
+    }
+
+    @RequestMapping(value = {"/dologin"}, method = RequestMethod.POST)
+    public String dologin(@Valid LoginCommand loginCommand, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "loginform";
+        }
+
+        return "redirect:/";
+    }
+}
